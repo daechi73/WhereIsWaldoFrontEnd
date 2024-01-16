deb9ff12f24ff86d892535b368ea1e7addd0ea81
J <59581793+daechi73@users.noreply.github.com>
2024-01-15 20:30:12 -0500

    WaldoFaceLoading testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./components/pages/Home";
+import WaldoFaceLoading from "./components/global/WaldoFaceLoading";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <WaldoFaceLoading />
     </>
   );
 }
