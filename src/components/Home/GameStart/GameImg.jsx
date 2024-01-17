7986bbb1089b00a9dbe654268eae18c1e57f153c
J <59581793+daechi73@users.noreply.github.com>
2024-01-17 13:20:03 -0500

    charBox useRef used to reference CharacterBox

--- src/components/Home/GameStart/GameImg.jsx
+++ src/components/Home/GameStart/GameImg.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import "./GameImg.css";
+import { useRef } from "react";
+import CharacterBox from "./CharacterBox";
 
 function GameImg(props) {
-  const getCoordinatesOnImgClick = (e) => {
-    var rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const xRatio = x / e.target.offsetWidth;
-    const yRatio = y / e.target.offsetHeight;
-    console.log("waldo poistion ratio: x=" + `${xRatio};` + ` y=${yRatio}`);
-  };
   return (
     <div className="gameImg">
       <img
         className="gameImg-img"
         src={props.chosenGame.src}
         alt="gameImg"
-        onClick={getCoordinatesOnImgClick}
+        onClick={props.getCoordinatesOnImgClick}
       />
+      <CharacterBox charBox={props.charBox} chosenGame={props.chosenGame} />
     </div>
   );
 }
