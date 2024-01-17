1aba263bdc29b00375b17671cec576fc687aa7a3
J <59581793+daechi73@users.noreply.github.com>
2024-01-16 20:51:47 -0500

    moved getCoordinateOnImgClick from GameStart to GameImg

--- src/components/Home/GameStart/GameImg.jsx
+++ src/components/Home/GameStart/GameImg.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./GameImg.css";
 
 function GameImg(props) {
+  const getCoordinatesOnImgClick = (e) => {
+    var rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const xRatio = x / e.target.offsetWidth;
+    const yRatio = y / e.target.offsetHeight;
+    console.log("waldo poistion ratio: x=" + `${xRatio};` + ` y=${yRatio}`);
+  };
   return (
     <div className="gameImg">
       <img
         className="gameImg-img"
         src={props.chosenGame.src}
         alt="gameImg"
-        onClick={props.getCoordinatesOnImgClick}
+        onClick={getCoordinatesOnImgClick}
       />
     </div>
   );
--- src/components/Home/GameStart/GameStart.jsx
+++ src/components/Home/GameStart/GameStart.jsx
@@ -2,20 +2,9 @@ import React from "react";
 import GameImg from "./GameImg";
 
 function GameStart(props) {
-  const getCoordinatesOnImgClick = (e) => {
-    var rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const xRatio = x / e.target.offsetWidth;
-    const yRatio = y / e.target.offsetHeight;
-    console.log("waldo poistion ratio: x=" + `${xRatio};` + ` y=${yRatio}`);
-  };
   return (
     <div className="gameStart">
-      <GameImg
-        getCoordinatesOnImgClick={getCoordinatesOnImgClick}
-        chosenGame={props.chosenGame}
-      />
+      <GameImg chosenGame={props.chosenGame} />
     </div>
   );
 }
