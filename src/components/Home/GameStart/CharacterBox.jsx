4cb830be74ae56c812101dbacb64b588d9e95de7
J <59581793+daechi73@users.noreply.github.com>
2024-01-17 22:56:51 -0500

    passing in props

--- src/components/Home/GameStart/CharacterBox.jsx
+++ src/components/Home/GameStart/CharacterBox.jsx
@@ -4,7 +4,14 @@ import CharacterBoxCard from "./CharacterBoxCard";
 
 function CharacterBox(props) {
   const renderCharacterBoxCard = props.chosenGame.characters.map((e, i) => {
-    return <CharacterBoxCard key={i} src={e.src} charName={e.char} />;
+    return (
+      <CharacterBoxCard
+        key={i}
+        char={e}
+        userClickCoords={props.userClickCoords}
+        charBox={props.charBox}
+      />
+    );
   });
   return (
     <div className="characterBox" ref={props.charBox}>
