ab6ddc385cfab5c0ee8dab91b4077e40fb7ea4ed
J <59581793+daechi73@users.noreply.github.com>
2024-01-17 23:02:07 -0500

    clicking characterBoxCard will check if the userClick and char position match

--- src/components/Home/GameStart/CharacterBoxCard.jsx
+++ src/components/Home/GameStart/CharacterBoxCard.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./CharacterBoxCard.css";
+
 function CharacterBoxCard(props) {
+  const handleCharBoxCardClick = (e) => {
+    console.log(checkPositions());
+    props.charBox.current.style.display = "none";
+  };
+  const checkPositions = () => {
+    if (
+      props.char.top < props.userClickCoords.current.y &&
+      props.char.bottom > props.userClickCoords.current.y &&
+      props.char.left < props.userClickCoords.current.x &&
+      props.char.right > props.userClickCoords.current.x
+    )
+      return true;
+    return false;
+  };
+
   return (
-    <div className="characterBoxCard">
+    <div className="characterBoxCard" onClick={handleCharBoxCardClick}>
       <img
         className="characterBoxCard-img"
-        src={props.src}
+        src={props.char.src}
         alt={"CharacterBoxCard "}
       />
-      <div className="characterBoxCard-name">{props.charName}</div>
+      <div className="characterBoxCard-name">{props.char.char}</div>
     </div>
   );
 }
