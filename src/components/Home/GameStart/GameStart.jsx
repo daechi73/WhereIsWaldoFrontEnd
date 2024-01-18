0efbd464f2619380549978981adb3d7e22f7ce93
J <59581793+daechi73@users.noreply.github.com>
2024-01-17 22:59:52 -0500

    moved all the functions used into its own module

--- src/components/Home/GameStart/GameStart.jsx
+++ src/components/Home/GameStart/GameStart.jsx
@@ -1,30 +1,19 @@
 import React from "react";
 import GameImg from "./GameImg";
 import { useRef } from "react";
+import GameStartMethods from "../../GameSetupModules/GameStartMethods";
 
 function GameStart(props) {
   const charBox = useRef();
-  const makeCharBoxAppear = (e) => {
-    charBox.current.style.display = "block";
-    charBox.current.style.top = e.pageY + "px";
-    charBox.current.style.left = e.pageX + "px";
-    //charBox.current.style.cssText = `top:10%;position:absolute;display:block;`;
-  };
-  const getCoordinatesOnImgClick = (e) => {
-    var rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const xRatio = x / e.target.offsetWidth;
-    const yRatio = y / e.target.offsetHeight;
-    makeCharBoxAppear(e);
-    console.log("waldo poistion ratio: x=" + `${xRatio};` + ` y=${yRatio}`);
-  };
+  const userClickCoords = useRef(null);
+  const gameStartMethods = GameStartMethods(charBox, userClickCoords);
   return (
     <div className="gameStart">
       <GameImg
         chosenGame={props.chosenGame}
-        getCoordinatesOnImgClick={getCoordinatesOnImgClick}
+        handleGameImgClick={gameStartMethods.handleGameImgClick}
         charBox={charBox}
+        userClickCoords={userClickCoords}
       />
     </div>
   );
